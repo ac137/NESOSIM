4afbcbb77bf970d92dc13c62cf0cc9c4c01d4be4
ac <7801186+ac137@users.noreply.github.com>
2020-06-16 16:22:02 -0400

    change file paths, etc

--- source/config.py
+++ source/config.py
@@ -1,10 +1,18 @@
 
 #!/usr/bin/env python
 
+# osisaf_raw_path = '/Volumes/PETTY_E/DATA/ICE_DRIFT/OSISAF-MERGED/'
+# cdr_raw_path = '/Volumes/PETTY_E/DATA/ICE_CONC/CDR/'
+# reanalysis_raw_path = '/Volumes/PETTY_E/DATA/REANALYSES/'
+# forcing_save_path = '/home/alex/modeldev/NESOSIM/Forcings/'
+# model_save_path = '/home/alex/modeldev/NESOSIM/Output/'
+# figure_path = '/home/alex/modeldev/NESOSIM/Figures/'
+# oib_data_path='/users/aapetty/Data/IceBridge/thickness/'
+
 osisaf_raw_path = '/Volumes/PETTY_E/DATA/ICE_DRIFT/OSISAF-MERGED/'
 cdr_raw_path = '/Volumes/PETTY_E/DATA/ICE_CONC/CDR/'
-reanalysis_raw_path = '/Volumes/PETTY_E/DATA/REANALYSES/'
-forcing_save_path = '/Volumes/PETTY_E/NESOSIM/Forcings/'
-model_save_path = '/Volumes/PETTY_E/NESOSIM/Output/'
-figure_path = '/Volumes/PETTY_E/NESOSIM/Figures/'
+reanalysis_raw_path = '/data/kushner_group/'
+forcing_save_path = '/users/jk/18/acabaj/NESOSIM/Forcings/'
+model_save_path = '/users/jk/18/acabaj/NESOSIM/Output/'
+figure_path = '/users/jk/18/acabaj/NESOSIM/Figures/'
 oib_data_path='/users/aapetty/Data/IceBridge/thickness/'
