aaec720d281b745cba6de0db8cd382d28edc1f86
alex cabaj <acabaj@physics.utoronto.ca>
2020-07-06 14:04:32 -0400

    config for animus server

--- source/config.py
+++ source/config.py
@@ -10,7 +10,7 @@
 # oib_data_path='/users/aapetty/Data/IceBridge/thickness/'
 
 osisaf_raw_path = '/Volumes/PETTY_E/DATA/ICE_DRIFT/OSISAF-MERGED/'
-cdr_raw_path = '/Volumes/PETTY_E/DATA/ICE_CONC/CDR/'
+cdr_raw_path = '/users/jk/17/acabaj/CDR/'
 reanalysis_raw_path = '/data/kushner_group/'
 forcing_save_path = '/users/jk/18/acabaj/NESOSIM/Forcings/'
 model_save_path = '/users/jk/18/acabaj/NESOSIM/Output/'
