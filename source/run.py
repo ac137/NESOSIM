644e7661e9abdf45443d66ed9d8a4f339b93e2e0
alex cabaj <acabaj@physics.utoronto.ca>
2020-07-16 17:27:27 -0400

    add loop to run

--- source/run.py
+++ source/run.py
@@ -21,26 +21,32 @@ import sys
 from config import forcing_save_path
 from config import model_save_path
 from config import figure_path
+import NESOSIM
+from calendar import monthrange
 
-yearS=2019
+yearS=2012
 monthS=8 # September = 8
 dayS=0
 
-yearE=2020
-monthE=2
-from calendar import monthrange
-dayE=monthrange(yearE, monthE)[1]
+yearE=2013
+monthE=4
+#dayE=monthrange(yearE, monthE)[1]
 
+for y in range(2010,2017):
+	yearS = y
+	yearE = y+1
 
-import NESOSIM	
-NESOSIM.main(year1=yearS, month1=monthS, day1=dayS, year2=yearE, month2=monthE-1, day2=dayE-1,
+	dayE=monthrange(yearE, monthE)[1]
+
+	print('yearS = {}'.format(yearS))
+	NESOSIM.main(year1=yearS, month1=monthS, day1=dayS, year2=yearE, month2=monthE-1, day2=dayE-1,
 	outPathT=model_save_path, 
 	forcingPathT=forcing_save_path, 
 	figPathT=figure_path+'Model/',
 	precipVar='ERA5', windVar='ERA5', driftVar='OSISAF', concVar='CDR', 
 	densityTypeT='variable', extraStr='v11', outStr='', IC=2, 
 	windPackFactorT=5.8e-7, windPackThreshT=5, leadLossFactorT=5.8e-7,
-	dynamicsInc=1, leadlossInc=1, windpackInc=1,scaleCS=True)
+	dynamicsInc=1, leadlossInc=1, windpackInc=1,scaleCS=False)
 
 
 
