2033f75d66ba99c118100638897f69f11ef8d3be
alex cabaj <acabaj@physics.utoronto.ca>
2021-03-09 12:56:27 -0500

    adjust to also store rejected values

--- source/mcmc_nesosim.py
+++ source/mcmc_nesosim.py
@@ -8,8 +8,8 @@ np.random.seed(42)
 # default wpf 5.8e-7
 # default llf 2.9e-7 ? different default for multiseason
 
-ITER_MAX = 10 # start small
-UNCERT = 100 # obs uncertainty for log-likelihood (also can be used to tune)
+ITER_MAX = 2000 # start small
+UNCERT = 30 # obs uncertainty for log-likelihood (also can be used to tune)
 # par_vals = [1., 1.] #initial parameter values
 
 PAR_SIGMA = 1 # standard deviation for parameter distribution
@@ -28,8 +28,12 @@ print(stats_0)
 par_list = [par_vals]
 loglike_list = [p0]
 stats_list = [stats_0] # collect rmse and r also, etc.
-var_cond_list=[]
-diff_list=[]
+#var_cond_list=[]
+#diff_list=[]
+rejected_pars = []
+rejected_lls = []
+rejected_stats = []
+
 
 # first just try metropolis (don't reject proposed values of params)
 
@@ -67,14 +71,14 @@ for i in range(ITER_MAX):
 		# in log space, p/q becomes p - q, so check difference here
 		# checking with respect to uniform distribution
 		var_cond = np.log(np.random.rand())
-		var_cond_list.append(var_cond)
-		diff = p-p0
-		diff_list.append(diff)
+#		var_cond_list.append(var_cond)
+#		diff = p-p0
+#		diff_list.append(diff)
 		if p-p0 > var_cond:
 			# accept value
 			print('accepted value')
 			acceptance_count += 1
-			print('acceptance rate: {}/{} = {}'.format(acceptance_count,i+1,acceptance_count/float(i+1)))
+#			print('acceptance rate: {}/{} = {}'.format(acceptance_count,i+1,acceptance_count/float(i+1)))
 			par_vals = par_new
 			p0 = p
 			# append to list/ possibly save these to disk so that interrupting the
@@ -83,10 +87,20 @@ for i in range(ITER_MAX):
 			par_list.append(par_vals)
 			loglike_list.append(p0)
 			stats_list.append(stats)
+		else:
+			print('rejected value')
+			rejected_pars.append(par_new)
+			rejected_lls.append(p)
+			rejected_stats.append(stats)
+
+		print('acceptance rate: {}/{} = {}'.format(acceptance_count,i+1,acceptance_count/float(i+1)))
 
 # save like this for now, save more nicely later			
 np.savetxt('par_vals.txt',par_list)
 np.savetxt('log_likelihoods.txt',loglike_list)
 np.savetxt('stat_vals.txt',np.array(stats_list)) 
-np.savetxt('var_cond.txt',var_cond_list)
-np.savetxt('diff.txt',diff_list)
+#np.savetxt('var_cond.txt',var_cond_list)
+#np.savetxt('diff.txt',diff_list)
+np.savetxt('rejected_pars.txt',rejected_pars)
+np.savetxt('rejected_lls.txt',rejected_lls)
+np.savetxt('rejected_stats.txt',rejected_stats)
