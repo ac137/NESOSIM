53d43b85c1b0dab24a0bd1e79c1bb2abf93a5711
Alex Cabaj <Alex.Cabaj@ec.gc.ca>
2025-03-19 10:54:09 -0400

    script for running model for full year; doesn't differ much from run_oneseason but separated for ease of use

---
+++ source/run_full_year.py
@@ -0,0 +1,55 @@
+""" run_oneseason.py
+	
+	Run script for the NESOSIM model included in NESOSIM.py 
+	Model written by Alek Petty (03/01/2018)
+	Contact me for questions (alek.a.petty@nasa.gov) or add a query to the GitHub repo (www.github.com/akpetty/NESOSIM)
+
+	Update history:
+		03/01/2018: Version 1
+
+"""
+
+import matplotlib
+matplotlib.use("AGG")
+from pylab import *
+
+
+import subprocess
+import shlex
+import sys
+
+from config import forcing_save_path
+from config import model_save_path
+from config import figure_path
+
+
+forcing_save_path = '../forcings_full_year/'
+# forcing_save_path = r'C:\Users\CabajA\forcings_full_year'
+print('Forcing file path:', forcing_save_path)
+print('Output path:', model_save_path)
+print('Figure save path:', figure_path)
+
+yearS=2019
+monthS=8 # August = 7
+dayS=0
+
+yearE=2020
+monthE=7 # April = 7
+dayE=30 # end day off by 1
+
+print(yearS, monthS, dayS, yearE, monthE, dayE)
+
+import NESOSIM	
+NESOSIM.main(year1=yearS, month1=monthS, day1=dayS, year2=yearE, month2=monthE, day2=dayE,
+	outPathT=model_save_path, 
+	forcingPathT=forcing_save_path, 
+	figPathT=figure_path,
+	precipVar='ERA5', windVar='ERA5', driftVar='NSIDCv4', concVar='CDR', 
+	icVar='ERA5', densityTypeT='variable', extraStr='v11', outStr='test_summer_lin_2', IC=2, 
+	windPackFactorT=5.8e-7, windPackThreshT=5, leadLossFactorT=2.9e-7,
+	dynamicsInc=1, leadlossInc=1, windpackInc=1,atmlossInc=1,scaleCS=True, dx=100000,
+	plotdaily=0)
+
+
+
+
